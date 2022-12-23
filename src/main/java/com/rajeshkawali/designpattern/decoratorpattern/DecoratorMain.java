07d679cadab9e944c28e83c54b9edf3536812f41
rajesh_kawali <Rajesh_Kawali@Dellteam.com>
2022-12-23 13:30:15 +0530

    Decorator design pattern

--- src/main/java/com/rajeshkawali/designpattern/decoratorpattern/DecoratorMain.java
+++ src/main/java/com/rajeshkawali/designpattern/decoratorpattern/DecoratorMain.java
@@ -16,3 +16,14 @@ public class DecoratorMain {
 		System.out.println(food.prepareFood());
 	}
 }
+
+/*
+Decorator design pattern allows us to dynamically add functionality and behavior 
+to an object without affecting the behavior of other existing objects within the same class. 
+We use inheritance to extend the behavior of the class. This takes place at compile-time, 
+and all the instances of that class get the extended behavior.
+
+1.Decorator design pattern is useful in providing runtime modification abilities and hence more flexible. Its easy to maintain and extend when the amount of choices are more.
+2.The disadvantage of decorator design pattern is that it uses plenty of similar kind of objects (decorators)
+3.Decorator pattern is used a lot in Java IO classes, like FileReader, BufferedReader, etc.
+*/
