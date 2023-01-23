e6b48c10bc7dc22f860e860af7483b0d8598a642
KAWALI <rajesh_kawali@dellteam.com>
2023-01-23 20:48:52 +0530

    Design patterns

--- src/main/java/com/rajeshkawali/designpattern/observerpattern/Channel.java
+++ src/main/java/com/rajeshkawali/designpattern/observerpattern/Channel.java
@@ -8,7 +8,15 @@ import java.util.List;
  *
  */
 public class Channel implements Subject {
+	/*
+	The Observer design pattern is a behavioral pattern that allows an object, 
+	called the subject, to notify other objects, called observers, about changes in its state. 
+	The Observer pattern is useful when you want to create one-to-many relationships between objects, 
+	where one subject object is associated with multiple observer objects, 
+	and when you want to be able to notify multiple observer objects 
+	about changes in the subject object's state.
 
+	*/
 	private List<Subscriber> subscribers;
 	// removed private from String title just to access the values in main class
 	String title;
--- src/main/java/com/rajeshkawali/designpattern/observerpattern/YoutubeMainClass.java
+++ src/main/java/com/rajeshkawali/designpattern/observerpattern/YoutubeMainClass.java
@@ -6,6 +6,16 @@ package com.rajeshkawali.designpattern.observerpattern;
  */
 public class YoutubeMainClass {
 
+	/*
+	Observer is a behavioral design pattern that lets you define a 
+	subscription mechanism to notify multiple objects about any events 
+	that happen to the object they are observing.
+
+	Observer pattern is used when there is one-to-many relationship between objects 
+	such as if one object is modified, its depenedent objects are to be notified automatically. 
+	Observer pattern falls under behavioral pattern category.
+
+	*/
 	public static void main(String[] args) {
 
 		Channel channel = new Channel();
@@ -36,13 +46,22 @@ public class YoutubeMainClass {
 	}
 }
 
+
+
 /*
-Observer is a behavioral design pattern that lets you define a 
-subscription mechanism to notify multiple objects about any events 
-that happen to the object they’re observing.
+The Observer design pattern is a behavioral design pattern that allows objects to be 
+notified of changes in the state of other objects. In the context of a YouTube example, 
+the observer pattern can be used to notify subscribers of a channel about new notifications, 
+such as new videos being uploaded or live streams starting.
 
-Observer pattern is used when there is one-to-many relationship between objects 
-such as if one object is modified, its depenedent objects are to be notified automatically. 
-Observer pattern falls under behavioral pattern category.
+Here is an example of how the observer pattern could be implemented in a YouTube application:
 
+The Channel class represents a YouTube channel and contains information such as the channel's name and the videos it has uploaded.
+The Subscriber class represents a user who has subscribed to a channel.
+The Notification class represents a notification that is sent to subscribers, such as a new video being uploaded or a live stream starting.
+The Channel class would have a list of Subscriber objects that are subscribed to it. When a new video is uploaded or a live stream starts, the Channel class would create a new Notification object and then notify all of its subscribers by calling a method on the Subscriber objects, passing the Notification object as an argument.
+
+The Subscriber class would have a method that is called by the Channel 
+class when a new notification is sent. This method would take the Notification 
+object as an argument and handle it accordingly, for example, by displaying a message to the user.
 */
